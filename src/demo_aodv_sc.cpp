5f1c153a7ef761b813b8fc71198da27919a92b1d
ktiggos <k.tiggos@gmail.com>
2026-02-28 21:56:02 +0200

    Add demo cpp for AODV protocol simulation

---
+++ src/demo_aodv_sc.cpp
@@ -0,0 +1,12 @@
+#include "vcu.hpp"
+#include "sc_s1.hpp"
+#include "sc_s2.hpp"
+
+typedef VCU VirtucalControlUnit;
+
+int main(int argc, char* argv[]) {
+	uint8_t n1{1}, n2{2}, n3{3};
+	VCU u1(n1), u2(n2), u3(n3);
+
+	return 0;
+}
