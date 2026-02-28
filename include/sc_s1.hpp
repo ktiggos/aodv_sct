4f32cde5909aad65eba6b6e5bb24aaf6a2cba460
ktiggos <k.tiggos@gmail.com>
2026-02-28 19:43:23 +0200

    sc_s1.hpp: Add priv and pub 'get' functions

--- include/sc_s1.hpp
+++ include/sc_s1.hpp
@@ -5,15 +5,16 @@
 class SupervisorS1 {
 	public:
 		explicit SupervisorS1(uint8_t node);
-
+		std::string getState() const;
 	private:
 		enum class State : uint8_t {
-			qs1,
-			qs2
-		};
+                        qs1,
+                        qs2
+                };
+
+		void state_transition(const Event& ev);
+		std::string getStateName(const State st) const;
 
 		uint8_t node_;
 		State st_{State::qs1};
-
-		void state_transition(const Event& ev);
 };
