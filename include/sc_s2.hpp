c0742836547a43e4e3fd3c5d5afc3f61bbbb476d
ktiggos <k.tiggos@gmail.com>
2026-02-28 21:00:00 +0200

    Add sc_s2.hpp header for supervisor2 implementation

---
+++ include/sc_s2.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "events.hpp"
+#include <string>
+
+class SupervisorS2 {
+        public:
+                explicit SupervisorS2(uint8_t node);
+                std::string get_state() const;
+                void send_event(const Event& ev);
+        private:
+                enum class State : uint8_t {
+                        qs1,
+                        qs2
+                };
+
+                void state_transition(const Event& ev);
+                std::string get_state_name(const State st) const;
+
+                uint8_t node_;
+                State st_{State::qs1};
+};
