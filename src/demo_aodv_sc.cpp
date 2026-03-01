86b042ff60e04efdc861b7e72710eca5fd5accae
ktiggos <k.tiggos@gmail.com>
2026-03-01 10:27:36 +0200

    demo_aodv_sc: Read json file

--- src/demo_aodv_sc.cpp
+++ src/demo_aodv_sc.cpp
@@ -2,10 +2,24 @@
 #include "sc_s1.hpp"
 #include "sc_s2.hpp"
 
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
 typedef VirtualControlUnit VCU;
 
 int main(int argc, char* argv[]) {
-	VCU u1(1), u2(2), u3(3);
+	VCU units[3] = {VCU(1), VCU(2), VCU(3)};
+
+	std::ifstream file("data/event_seq.json");
+
+	if(!file){
+		throw::std::runtime_error("Cannot open json file.");
+	};
+
+	nlohmann::json ev_seq;
+	file >> ev_seq;`
 
 	return 0;
 }
