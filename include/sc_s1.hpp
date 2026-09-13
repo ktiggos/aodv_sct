6c02fd87b65f1b4cf68fdde8b2628588ad6e63b9
ktiggos <k.tiggos@gmail.com>
2026-09-13 22:28:15 +0300

    sc_s1: Refactor to derived class from base

--- include/sc_s1.hpp
+++ include/sc_s1.hpp
@@ -1,21 +1,12 @@
 #pragma once
 #include "events.hpp"
 #include <string>
+#include "supervisor.hpp"
 
-class SupervisorS1 {
-	public:
-		explicit SupervisorS1(uint8_t node);
-		std::string get_state() const;
-		void send_event(const Event& ev);
-	private:
-		enum class State : uint8_t {
-                        qs1,
-                        qs2
-                };
-
-		void state_transition(const Event& ev);
-		std::string get_state_name(const State st) const;
-
-		uint8_t node_;
-		State st_{State::qs1};
+class SupervisorS1 : public Supervisor {
+    public:
+        explicit SupervisorS1(uint8_t node);
+    private:
+        void state_transition(const Event& ev) override;
+        void init_super() override;
 };
--- src/sc_s1.cpp
+++ src/sc_s1.cpp
@@ -1,8 +1,12 @@
 #include "sc_s1.hpp"
 #include <iostream>
 
-SupervisorS1::SupervisorS1(uint8_t node) : node_(node) {
-	std::cout<<"Node "<<(int)node_<<" - S1 Created"<<"\n";
+SupervisorS1::SupervisorS1(uint8_t node) : Supervisor(node) {
+	this->init_super();
+}
+
+void SupervisorS1::init_super() {
+	std::cout<<"Node "<<(int)this->node_<<" - S1 Created"<<"\n";
 }
 
 void SupervisorS1::state_transition(const Event& ev){
@@ -28,21 +32,5 @@ void SupervisorS1::state_transition(const Event& ev){
 			};
 			break;
 	}
-}
-
-std::string SupervisorS1::get_state_name(const State st) const {
-	switch (st) {
-        	case State::qs1: return "qs1";
-        	case State::qs2: return "qs2";
-    	}
-    	return "unknown";
-}
-
-std::string SupervisorS1::get_state() const {
-	return this->get_state_name(this->st_);
-}
-
-void SupervisorS1::send_event(const Event& ev) {
-        this->state_transition(ev);
 }
 
